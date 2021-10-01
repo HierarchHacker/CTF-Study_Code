f175143f52cf0b7ef678f98c348d134f9529929b
Hierarch <hierarchthurs@gmail.com>
2021-10-01 17:00:10 +0800

    C language

---
+++ Ctest/cmake-build-debug/CMakeFiles/Ctest.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Ctest.dir/main.c.obj"
+  "Ctest.exe"
+  "Ctest.exe.manifest"
+  "Ctest.pdb"
+  "libCtest.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Ctest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
